5487552555bafc7dfc1f68c3e27ed55eab3f25bf
Alexey Bezhan <al@fueledbylemons.com>
2015-09-08 15:04:59 +0100

    Switch to ci stage on old AWS account

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,5 +4,5 @@ export ANSIBLE_CONFIG=playbooks/ansible.cfg
 export CREDENTIALS_PATH=$PWD/$1
 shift
 
-ansible-playbook -i playbooks/hosts playbooks/jenkins_playbook.yml --private-key ${CREDENTIALS_PATH}/aws-keys/development.pem \
+ansible-playbook -i playbooks/hosts playbooks/jenkins_playbook.yml --private-key ${CREDENTIALS_PATH}/aws-keys/ci.pem \
     -e "@${CREDENTIALS_PATH}/jenkins-vars/jenkins.yml" $@
